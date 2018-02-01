4dd32c81bf944916973717bf806acd3374ebbeae
Crazy Lama <lama23@Crazys-Mac-mini.local>
2018-02-01 14:33:01 +0100

    try like

--- loop_check.py
+++ loop_check.py
@@ -15,8 +15,7 @@ try:
     session.login()
 
     # settings
-    session.set_upper_follower_count(limit=2500)
-    session.set_do_comment(True, percentage=10)
+    session.set_do_comment(True, percentage=100)
     session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
     session.set_dont_include(['friend1', 'friend2', 'friend3'])
     session.set_dont_like(['pizza', 'girl'])
